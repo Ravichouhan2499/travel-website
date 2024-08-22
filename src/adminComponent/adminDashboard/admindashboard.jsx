45fec27e2b4309174020617e0406dca469181425
Ravi Chouhan <ravichouhan2499@gmail.com>
2024-08-22 10:37:05 +0530

    relative path

--- src/adminComponent/adminDashboard/admindashboard.jsx
+++ src/adminComponent/adminDashboard/admindashboard.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './admindashboard.css'
 import AdService from '../adService/adServices'
 import ViewServices from '../adService/viewAdService'
 import ViewTopDestiny from '../topDestiny/viewAdDestiny'
 import ViewBlog from '../blog/viewBlog'
 import ViewPackages from '../adPackages/viewAdPackages'
+import { useLocation } from 'react-router-dom'
 
 export default function Admindashboard() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+      if (location.pathname === '/admin/') {
+          const link = document.createElement('link');
+          link.rel = 'stylesheet';
+          link.href = '/css/bootstrap.min.css';
+          document.head.appendChild(link);
+
+          return () => {
+              document.head.removeChild(link);
+          };
+      }
+  }, [location]);
+
+
+
   return (
     <div className='dash' style={{ padding: '0 15px' }}>
     <div className='section' style={{ marginBottom: '30px' }}>
