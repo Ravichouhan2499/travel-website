062fba53a48e72b32ee4c561271f9e020075ac96
Ravi Chouhan <ravichouhan2499@gmail.com>
2024-09-13 10:52:10 +0530

    fix the bugs

--- src/Components/CaresueolComponenet/Caro.jsx
+++ src/Components/CaresueolComponenet/Caro.jsx
@@ -31,7 +31,7 @@ export default function Caro() {
         <Carousel.Caption className="carousel-caption d-flex flex-column align-items-center justify-content-center">
         <div className="p-3 text-center">
             <h5 className="text-white text-uppercase mb-3 ">AVANTIKA &amp; VACATIONS</h5>
-            <h1 className="display-4 text-white mb-md-4 ">Let's Discover The World Together </h1>
+            <h1 className="display-4 text-white mb-md-4 ">Let's Discover  <br/> The World Together </h1>
             <Link to="/contact" className="btn btn-primary py-2 px-4 me-3 animated slideInLeft">Book now</Link>
           </div>
         </Carousel.Caption>
